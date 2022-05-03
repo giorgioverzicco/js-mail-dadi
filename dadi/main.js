7b40c7b620fa0adc43b18270d86d20b15c9396de
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-05-03 15:01:43 +0200

    add ui to dices

--- dadi/main.js
+++ dadi/main.js
@@ -6,10 +6,24 @@ for (let i = 0; i < 2; i++) {
 const user = numbers[0];
 const cpu = numbers[1];
 
+const userElement = document.querySelector(".user");
+userElement.innerHTML = user;
+
+const cpuElement = document.querySelector(".cpu");
+cpuElement.innerHTML = cpu;
+
+const title = document.querySelector(".title");
+
 if (user === cpu) {
-  alert("Pareggio!");
+  // alert("Pareggio!");
+  console.log("Pareggio!");
+  title.innerHTML = "Pareggio!";
 } else if (user > cpu) {
-  alert("Hai vinto!");
+  // alert("Hai vinto!");
+  console.log("Hai vinto!");
+  title.innerHTML = "Hai vinto!";
 } else {
-  alert("Hai perso!");
+  // alert("Hai perso!");
+  console.log("Hai perso!");
+  title.innerHTML = "Hai perso!";
 }

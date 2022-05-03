f349b82d16ad7ceb5c0aec15b79870567de94602
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-05-03 13:54:01 +0200

    refactor mail check with better array method

--- mail/main.js
+++ mail/main.js
@@ -1,9 +1,5 @@
 const authorizedEmails = ["samumadrigali@gmail.com", "nikmosca@gmail.com", "marcoacciarri@gmail.com"];
 const userEmail = prompt("Digita la tua email:");
+const isAuthorized = authorizedEmails.some((mail) => mail === userEmail);
 
-for (let i = 0; i < authorizedEmails.length; i++) {
-  if (userEmail === authorizedEmails[i]) {
-    alert("Sei autorizzato ad accedere!");
-    break;
-  }
-}
+alert(isAuthorized ? "Sei autorizzato ad accedere!" : "Non sei autorizzato ad accedere!");
